8f5903b4bb5ed4567782f0a5e815577d1c28c17a
Yulia <yulia.bosher@gmail.com>
2025-12-24 16:43:53 +0000

    Default configurations

---
+++ backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp -r /var/ossec/etc/ossec.conf /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh
+cp -r /var/ossec/etc/rules/local_rules.xml /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh
+cp -r /var/ossec/etc/shared/default/agent.conf /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh
+
---
+++ restore.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp -r /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh/ossec.conf /var/ossec/etc/ossec.conf
+cp -r /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh/local_rules.xml /var/ossec/etc/rules/local_rules.xml
+cp -r /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh/agent.conf /var/ossec/etc/shared/default/agent.conf
+
