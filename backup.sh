95b251842bed6ee9e2c03b94662a53ad84ab10a0
Yulia <yulia.bosher@gmail.com>
2025-12-29 16:37:21 +0000

    Added HTTP request to a PHP file upload

--- backup.sh
+++ backup.sh
@@ -2,4 +2,4 @@
 cp -r /var/ossec/etc/ossec.conf /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh
 cp -r /var/ossec/etc/rules/local_rules.xml /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh
 cp -r /var/ossec/etc/shared/default/agent.conf /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh
-
+cp -r /var/ossec/etc/ossec.conf /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh
--- restore.sh
+++ restore.sh
@@ -2,4 +2,4 @@
 cp -r /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh/ossec.conf /var/ossec/etc/ossec.conf
 cp -r /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh/local_rules.xml /var/ossec/etc/rules/local_rules.xml
 cp -r /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh/agent.conf /var/ossec/etc/shared/default/agent.conf
-
+cp -r /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh/ossec.conf /var/ossec/etc/ossec.conf
