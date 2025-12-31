ffcf98f435d3d10a0557077f3cb778907bad3d01
Yulia <yulia.bosher@gmail.com>
2025-12-31 13:43:40 +0000

    Added Archive creation of data directory in a shared folder rule

--- backup.sh
+++ backup.sh
@@ -3,3 +3,5 @@ cp -r /var/ossec/etc/ossec.conf /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and
 cp -r /var/ossec/etc/rules/local_rules.xml /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh
 cp -r /var/ossec/etc/shared/default/agent.conf /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh
 cp -r /var/ossec/etc/ossec.conf /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh
+cp -r /var/ossec/etc/lists/suspicious-programs /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh
+
--- restore.sh
+++ restore.sh
@@ -3,3 +3,4 @@ cp -r /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh/oss
 cp -r /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh/local_rules.xml /var/ossec/etc/rules/local_rules.xml
 cp -r /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh/agent.conf /var/ossec/etc/shared/default/agent.conf
 cp -r /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh/ossec.conf /var/ossec/etc/ossec.conf
+cp -r /home/yulia/FTP-and-SMB-File-Sharing-Attacks-and-Detection-Using-Wazuh/suspicious-programs /var/ossec/etc/lists/suspicious/programs
